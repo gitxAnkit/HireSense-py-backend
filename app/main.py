6371a876d8e6b6a50fbd38eda295b0dece707bc4
Ankit Verma <ankit.verma@sphinxworldbiz.com>
2026-06-07 11:35:12 -0700

    test

--- app/main.py
+++ app/main.py
@@ -8,7 +8,8 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI(title="HireSense API")
 
 app.include_router(job.router)
-
+ 
+ 
 @app.get("/")
 def root():
     return {"message": "HireSense Backend Running"}
